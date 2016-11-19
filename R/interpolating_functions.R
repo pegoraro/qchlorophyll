6f7e93352627cc295a6f272433dd0466864b8bbd
mick001 <michy.alice@quantide.com>
2016-11-19 01:57:47 +0100

    added interpolation function

--- R/interpolating_functions.R
+++ R/interpolating_functions.R
@@ -1,62 +1,87 @@
 ################################################################################
-#' Load nc files to be resized. Subfunction
+#' Interpolate a list of dplyr dataframes
 #'
 #' @param data_list a list of dplyr dataframe to interpolate
 #' @param reference_df a reference dataframe containing at least the following variables: longitude, latitude, id_pixel
-#' @param observation_id id of the observation: id_date for daily observations, month for monthly observations.
-#' @importFrom dplyr %>% tbl_df
+#' @param unique_id unique id of the observation (usually id_date or id_month)
+#' @param variable variable to interpolate
+#' @param coordinates_names names of the coordinates (longitude and latitude)
 #' @return a dplyr dataframe
 #' @export
 #'
-interpolate_grid <- function(data_list, reference_df, observation_id="id_date")
+interpolate_grid <- function(data_list, reference_df, unique_id = "id_date", variable = "qnet", coordinates_names = c("lon", "lat"))
 {
     # Lon-lat ranges
-    x_range <- as.numeric(c(min(reference_df$lon), max(reference_df$lon)))
-    y_range <- as.numeric(c(min(reference_df$lat), max(reference_df$lat)))
+    lon_range <- as.numeric(c(min(reference_df[lon_lat_names[1]]), max(reference_df[lon_lat_names[1]])))
+    lat_range <- as.numeric(c(min(reference_df[lon_lat_names[2]]), max(reference_df[lon_lat_names[2]])))
     step <- 0.1
 
-    # Either: interpolate each observation for each day, or?
-    # Generate grid
-    grd <- expand.grid(x = seq(from = x_range[1], to = x_range[2], by = step), y = seq(from = y_range[1], to = y_range[2], by = step))
-    # Set coordinates
-    coordinates(grd) <- ~x + y
-    gridded(grd) <- TRUE
+    fitted_df_list <- lapply(data_list, interpolate_single_grid,
+                             unique_id = unique_id,
+                             variable = variable,
+                             coordinates_names = coordinates_names,
+                             lon_range = lon_range,
+                             lat_range = lat_range,
+                             step = step)
 
-    unique_obs <- data_list[[1]] %>% select_("id_date") %>% distinct() %>% dim()
-    unique_obs <- unique_obs[1]
-    print(unique_obs)
+    fitted_df <- bind_rows(fitted_df_list)
 
-    fitted_df <- lapply(data_list, interpolate, grd = grd, unique_obs = unique_obs)
     return(fitted_df)
-
 }
 
-interpolate <- function(df, grd, unique_obs)
+################################################################################
+#' Interpolate each data grid.
+#' This function interpolates every datagrid day by day (or month by month in case
+#' of monthly observations)
+#'
+#' @param df a dplyr dataframe to interpolate
+#' @param unique_id unique id of the observation (usually id_date or id_month)
+#' @param variable variable to interpolate
+#' @param coordinates_names names of the coordinates (longitude and latitude)
+#' @param lon_range longitude range (min, max)
+#' @param lat_range latitude range (min, max)
+#' @param step step between longitude and latitude vectors
+#' @importFrom dplyr %>% select_ distinct filter mutate_ tbl_df bind_rows
+#' @importFrom sp coordinates gridded
+#' @importFrom gstat idw
+#' @return a dplyr dataframe
+#' @export
+#'
+interpolate_single_grid <- function(df, unique_id = "id_date", variable, coordinates_names, lon_range, lat_range, step)
 {
-    fitted_data <- list()
+    # Count the number of days/month to interpolate
+    days <- df %>% select_(unique_id) %>% distinct() %>% nrow()
+    # Output list
+    out_lst <- list()
 
-    unused <- df %>% select(id_date, month, year)
-    id_date_ <- as.numeric(unused["id_date"][1,])
-    month_ <- as.numeric(unused["month"][1,])
-    year_ <- as.numeric(unused["year"][1,])
-    print(paste("id_date", id_date_))
-
-    for(i in 1:unique_obs)
+    # Start to interpolate each date
+    for(i in 1:days)
     {
-        data <- df %>% filter(id_date == i) %>% na.omit()
-        data$x <- data$lon
-        data$y <- data$lat
-        coordinates(data) = ~x + y
-        idw <- idw(formula = qnet ~ 1, locations = data, newdata = grd)
-        idw.output <- as.data.frame(idw) %>% tbl_df()
-        names(idw.output)[1:3] <- c("lon", "lat", "qnet_interp")
-        idw_out <- tbl_df(idw.output) %>% mutate(id_date = id_date_, month = month_, year = year_)
-        fitted_data <- c(fitted_data, idw_out)
-        print(i)
+        # Select data from the ith date
+        day_data <- data %>% filter(id_date == i) %>% na.omit()
+        # Variables common to each date (id_date, month, year)
+        other_vars <- day_data %>%
+            select_(.dots = as.list(names(data)[names(data) != c(coordinates_names, var)])) %>%
+            distinct() %>% as.list()
+        # Set coordinates
+        coordinates(day_data) = ~lon + lat
+        # Set new grid, coordinates and gridded attribute
+        grd <- expand.grid(lon = seq(from = lon_range[1], to = lon_range[2], by = step), lat = seq(from = lat_range[1], to = lat_range[2], by = step))
+        coordinates(grd) <- ~lon + lat
+        gridded(grd) <- TRUE
+        # Interpolate using idw
+        idw <- idw(formula = as.formula(paste(var, 1, sep=" ~ ")), locations = day_data, newdata = grd)
+        idw_output <- as.data.frame(idw)
+        # Set names
+        names(idw_output)[1:3] <- c(coordinates_names[1], coordinates_names[2], var)
+        # Format output
+        idw_output <- idw_output %>%
+            tbl_df() %>%
+            select_(coordinates_names[1], coordinates_names[2], var) %>%
+            mutate_(.dots = other_vars)
+        # Assign output to list
+        out_lst[[i]] <- idw_output
     }
-
-    out <- do.call(rbind, fitted_data)
-    return(out)
+    # Bind all the dataframes in a single dataframe
+    out <- bind_rows(out_lst)
 }
-
-
